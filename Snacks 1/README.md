34a10bd36b35e011f788076027487c811a1df18c
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-28 18:48:31 +0200

    readme and js: snack 1

---
+++ Snacks 1/README.md
@@ -0,0 +1,11 @@
+## JSnack1
+
+Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+## Procedimento
+
+- inserisco un numero
+- **se** è pari
+  - stampo il numero
+- **altrimenti**
+  - stampa il numero successivo
--- Snacks 1/js/main.js
+++ Snacks 1/js/main.js
@@ -1,1 +1,13 @@
+//inserisco un numero
+const userNumber = parseInt(prompt("Inserisci un numero"));
 
+//se è pari
+if (userNumber % 2 == 0) {
+  //stampo il numero
+  console.log("Numero: " + userNumber);
+}
+//altrimenti
+else {
+  //stampa il numero successivo
+  console.log("Numero: " + (userNumber + 1));
+}
