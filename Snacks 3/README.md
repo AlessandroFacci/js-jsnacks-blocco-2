88143f4b2304a2cbfc9b55d8be25082db3f845e9
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-28 19:29:53 +0200

    readme and js: snack 3

---
+++ Snacks 3/README.md
@@ -0,0 +1,13 @@
+## JSnack3
+
+Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+## Procedimento
+
+- creo un array di numeri interi
+- dichiaro una variabile somma = 0
+- per ogni numero dell'array
+- recupero il numero correntemente iterato
+  - **se** è in posizione pari
+    - lo aggiungo alla variabile somma
+- stampo la somma
--- Snacks 3/js/main.js
+++ Snacks 3/js/main.js
@@ -1,1 +1,19 @@
+// creo un array di numeri interi
+const numbers = [2, 5, 7, 8, 3, 6, 10];
 
+// dichiaro una variabile somma = 0
+let sum = 0;
+
+// per ogni numero dell'array
+for (let i = 0; i < numbers.length; i++) {
+  // recupero il numero correntemente iterato
+  const currentNumber = numbers[i];
+
+  // se è in posizione dispari
+  if (i % 2 != 0) {
+    // lo aggiungo alla variabile somma
+    sum += currentNumber;
+  }
+}
+// stampo la somma
+console.log("somma: " + sum);
