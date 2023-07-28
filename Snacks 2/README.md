70c8f3e4d41b63fef4cddc5e7babfaacb4fe6081
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-07-28 19:13:53 +0200

    readme and js: snack 2

---
+++ Snacks 2/README.md
@@ -0,0 +1,17 @@
+## JSnack2
+
+Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+## Procedimento
+
+- dichiaro una lista di nomi
+- dichiaro una lista di cognomi
+- dichiaro una lista di finti invitati
+- dichiaro numero falsi invitati
+- finchè non ho abbastanza nomi e cognomi
+- genero un indice casuale nel range dell'array dei nomi
+- recuro un valore casuale dall'array dei nomi usando l'indice generato
+- genero un indice casuale nel range dell'array dei cognomi
+- recuro un valore casuale dall'array dei cognomi usando l'indice generato
+- concateno nome e cognome e lo pusho nell'array degli invitati
+- stampo l'array dei finti invitati
--- Snacks 2/js/main.js
+++ Snacks 2/js/main.js
@@ -1,1 +1,33 @@
+// dichiaro una lista di nomi
+const names = ["Mattia", "Matteo", "Erik", "Davide"];
 
+// dichiaro una lista di cognomi
+const surnames = ["Dalbon", "Pizzamiglio", "Lorenzi", "Sbampato"];
+
+// dichiaro una lista di finti invitati
+const fakeInvited = [];
+
+// dichiaro numero falsi invitati
+const numberFakeInvited = prompt("Quanti invitati vuoi generare?");
+
+// finchè non ho abbastanza nomi e cognomi
+for (let i = 0; i < numberFakeInvited; i++) {
+  // genero un indice casuale nel range dell'array dei nomi
+  const nameRandomIndex = Math.floor(Math.random() * names.length);
+
+  // recuro un valore casuale dall'array dei nomi usando l'indice generato
+  const randomName = names[nameRandomIndex];
+
+  // genero un indice casuale nel range dell'array dei cognomi
+  const surnameRandomIndex = Math.floor(Math.random() * surnames.length);
+
+  // recuro un valore casuale dall'array dei cognomi usando l'indice generato
+  const randomSurname = surnames[surnameRandomIndex];
+
+  // concateno nome e cognome e lo pusho nell'array degli invitati
+  const fakeGuest = randomName + " " + randomSurname;
+  fakeInvited.push(fakeGuest);
+}
+
+// stampo l'array dei finti invitati
+console.table(fakeInvited);
